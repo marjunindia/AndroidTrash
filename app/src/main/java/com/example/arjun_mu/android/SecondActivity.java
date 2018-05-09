8c3abfdbcc45e4ec6ed4655773971f38e82e56fd
PHOTONINT\arjun_mu <arjun.murali@photoninfotech.net>
2018-05-09 15:18:02 +0530

    activity lifecycle

--- app/src/main/java/com/example/arjun_mu/android/SecondActivity.java
+++ app/src/main/java/com/example/arjun_mu/android/SecondActivity.java
@@ -1,21 +1,17 @@
 package com.example.arjun_mu.android;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
+public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         showmsg("onCreate");
     }
 
@@ -44,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         showmsg("onStop");
+
     }
 
     @Override
@@ -61,10 +58,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void showmsg(String msg){
         Log.d("lifecycle"+TAG,msg);
-    }
-
-
-    public void movetoanother(View view) {
-        startActivity(new Intent(this, SecondActivity.class));
     }
 }
