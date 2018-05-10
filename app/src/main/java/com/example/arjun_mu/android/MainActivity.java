9501144b75ed2c18162aab5fcba7f0cbb926c148
PHOTONINT\arjun_mu <arjun.murali@photoninfotech.net>
2018-05-10 13:32:11 +0530

    Revert "my app to receive implicit intent"
    This reverts commit 32aa5c57e559c3ad38286a3a5be8b02e153c3b00.

--- app/src/main/java/com/example/arjun_mu/android/MainActivity.java
+++ app/src/main/java/com/example/arjun_mu/android/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.arjun_mu.android;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView=(TextView)findViewById(R.id.textview);
-        Intent intent = getIntent();
-        Uri uri = intent.getData();
-        if (uri != null) {
-            String uri_string = "URI: " + uri.toString();
-            textView.setText(uri_string);
-        }
-
-
     }
 }
