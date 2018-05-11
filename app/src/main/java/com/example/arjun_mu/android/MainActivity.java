73d4ba93db16fb7d63a2f70b4913904108111d0e
PHOTONINT\arjun_mu <arjun.murali@photoninfotech.net>
2018-05-11 11:46:07 +0530

    AsyncTaskLoader

--- app/src/main/java/com/example/arjun_mu/android/MainActivity.java
+++ app/src/main/java/com/example/arjun_mu/android/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +15,10 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
     EditText mBookInput;
     private TextView mTitleText;
@@ -30,35 +37,105 @@ public class MainActivity extends AppCompatActivity {
 
     public void searchBooks(View view) {
 
+        // Get the search string from the input field.
         String queryString = mBookInput.getText().toString();
 
-        // user enter word after hide keybord when user taps
+        // Hide the keyboard when the button is pushed.
         InputMethodManager inputManager = (InputMethodManager)
                 getSystemService(Context.INPUT_METHOD_SERVICE);
         inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
                 InputMethodManager.HIDE_NOT_ALWAYS);
+
+        // Check the status of the network connection.
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
-
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
+        // If the network is active and the search field is not empty,
+        // add the search term to the arguments Bundle and start the loader.
         if (networkInfo != null && networkInfo.isConnected() && queryString.length()!=0) {
-            new FetchBook(mTitleText, mAuthorText).execute(queryString);
             mAuthorText.setText("");
             mTitleText.setText("loading");
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle, this);
         }
-
+        // Otherwise update the TextView to tell the user there is no connection or no search term.
         else {
             if (queryString.length() == 0) {
                 mAuthorText.setText("");
-                mTitleText.setText("Please enter a search term");
+                mTitleText.setText("no_search_term");
             } else {
                 mAuthorText.setText("");
-                mTitleText.setText("Please check your network connection and try again.");
+                mTitleText.setText("no_network");
             }
         }
     }
 
 
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        return new BookLoader(this, args.getString("queryString"));
+    }
 
+
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        try {
+            // Convert the response into a JSON object.
+            JSONObject jsonObject = new JSONObject(data);
+            // Get the JSONArray of book items.
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            // Initialize iterator and results fields.
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            // Look for results in the items array, exiting when both the title and author
+            // are found or when all items have been checked.
+            while (i < itemsArray.length() || (authors == null && title == null)) {
+                // Get the current item information.
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                // Move to the next item.
+                i++;
+            }
+
+            // If both are found, display the result.
+            if (title != null && authors != null){
+                mTitleText.setText(title);
+                mAuthorText.setText(authors);
+                mBookInput.setText("");
+            } else {
+                // If none are found, update the UI to show failed results.
+                mTitleText.setText("no_results");
+                mAuthorText.setText("");
+            }
+
+        } catch (Exception e){
+            // If onPostExecute does not receive a proper JSON string, update the UI to show failed results.
+            mTitleText.setText("no_results");
+            mAuthorText.setText("");
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+    }
 }
