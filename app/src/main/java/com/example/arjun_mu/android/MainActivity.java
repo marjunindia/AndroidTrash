83f042b23798276647be13a259382d6c53720cb0
arjunarjun27 <arjunarjun27@gmail.com>
2018-05-13 17:06:25 +0530

    singleton class

--- app/src/main/java/com/example/arjun_mu/android/MainActivity.java
+++ app/src/main/java/com/example/arjun_mu/android/MainActivity.java
@@ -3,45 +3,39 @@ package com.example.arjun_mu.android;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    // EditText view for the website URI
-    private EditText mWebsiteEditText;
-    // EditText view for the location URI
-    private EditText mLocationEditText;
-    // EditText view for the share text
-    private EditText mShareTextEditText;
+
 
     private TextView globaltextview;
 
-    private GlobalVariable globalVariable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mWebsiteEditText = (EditText) findViewById(R.id.website_edittext);
-        mLocationEditText = (EditText) findViewById(R.id.location_editext);
-        mShareTextEditText = (EditText) findViewById(R.id.share_edittext);
+;
         globaltextview=(TextView)findViewById(R.id.apptextView);
-        globalVariable= (GlobalVariable) getApplicationContext();
+
 
 
     }
 
-
-
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        globalVariable.setUsername(globalVariable.getUsername());
+    protected void onResume() {
+        super.onResume();
+
+        if(MySingleton.getInstance().isSave()){
+            globaltextview.setText(MySingleton.getInstance().getName());
+
+        }
 
     }
 
     public void changetext(View view) {
-        globaltextview.setText(globalVariable.getUsername());
+        globaltextview.setText(MySingleton.getInstance().getName());
+            MySingleton.getInstance().setSave(true);
     }
 }
 
